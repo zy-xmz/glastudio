53867f521b0410df77b473a8209e39f0da8db3c3
zy-xmz <zy_20131114@163.com>
2022-03-24 05:51:31 +0800

    修改首页列表处理

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -6,20 +6,45 @@ class Chat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('聊天')),
-        body: ListView(
+        appBar: AppBar(
+          title: const Text('聊天', style: TextStyle(color: Color(0xFF323232))),
+          actions: [
+            IconButton(
+                onPressed: () => {},
+                icon: const Icon(Icons.add_circle_outline,
+                    color: Color(0xFF323232)))
+          ],
+          backgroundColor: Colors.white,
+        ),
+        body: ListView.separated(
             padding: const EdgeInsets.symmetric(vertical: 8),
-            children: [
-              for (int index = 1; index < 21; index++)
-                ListTile(
-                  leading: Image.network(
-                      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2F22%2Fc4%2F51%2F2a%2Fd49ad33d13d3801e719a8a2474613a09.jpg'),
-                  trailing: const Text('时间'),
-                  title: const Text('某月某日'),
-                  subtitle: const Text('就买这个吧'),
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                  // 左边内容
+                  leading: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.asset('lib/assets/img/head.jpg',
+                          width: 50, height: 50)),
+                  // 右边内容
+                  trailing: const Text('回复时间',
+                      style: TextStyle(color: Color(0xFFcccccc))),
+                  // 中间第一行标题
+                  title: const Text('标题',
+                      style: TextStyle(color: Color(0xFF323232))),
+                  // 中间第二行标题
+                  subtitle: const Text('副标题',
+                      style: TextStyle(color: Color(0xFF7f7f7f))),
+                  // 是否有选中效果
                   selected: true,
-                  onTap: () => {},
-                )
-            ]));
+                  // 点击事件
+                  onTap: () => {});
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(
+                    height: 10,
+                    thickness: 1,
+                    indent: 70,
+                    color: Color(0xFFeeeeee)),
+            itemCount: 20));
   }
 }
