30b453c04243791f8f3da2c741d7b8049f50024f
zy-xmz <zy_20131114@163.com>
2022-04-07 06:20:34 +0800

    修改发现页

--- lib/pages/discovery.dart
+++ lib/pages/discovery.dart
@@ -7,76 +7,126 @@ class Discovery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: CustomBar(title: '发现', actions: [
-          IconButton(
-              onPressed: () => {},
-              icon: const Icon(Icons.add_circle_outline,
-                  color: Color(0xFF323232)))
-        ]),
-        body: ListView(children: [
-          Container(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              color: Colors.white,
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(children: [
-                      const Icon(Icons.people),
-                      Container(
-                        margin: const EdgeInsets.only(left: 10),
-                        child: const Text('朋友圈'),
-                      ),
-                    ]),
-                    Row(
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.only(right: 10),
-                          child: ClipRRect(
-                            child: Image.asset(
-                              'lib/assets/img/head.jpg',
-                              width: 30,
-                              height: 30,
-                            ),
-                            borderRadius: BorderRadius.circular(4),
-                          ))
-                        ,
-                        const Text('赞过'),
-                        const Icon(Icons.chevron_right)
-                      ],
-                    )
-                  ])),
-          Container(
-            margin: const EdgeInsets.only(top: 10),
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            color: Colors.white,
-            child: Row(
+      appBar: CustomBar(title: '发现', actions: [
+        IconButton(
+            onPressed: () => {},
+            icon:
+                const Icon(Icons.add_circle_outline, color: Color(0xFF323232)))
+      ]),
+      body: ListView(children: [
+        Container(
+          color: Colors.white,
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(children: const [Icon(Icons.camera), Text('朋友圈')]),
+              Row(children: [
+                ClipRRect(
+                  child: Image.asset(
+                    'lib/assets/img/head.jpg',
+                    width: 30,
+                    height: 30,
+                  ),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                const Icon(Icons.chevron_right)
+              ])
+            ],
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 10),
+          color: Colors.white,
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+          child: Column(
+            children: [
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Row(children: [
-                    const Icon(Icons.video_camera_front),
-                    Container(
-                      margin: const EdgeInsets.only(left: 10),
-                      child: const Text('视频号'),
-                    ),
+                  Row(children: const [
+                    Icon(Icons.play_circle_outline),
+                    Text('视频号')
                   ]),
-                  Row(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(right: 10),
-                        child: ClipRRect(
-                          child: Image.asset(
-                            'lib/assets/img/head.jpg',
-                            width: 30,
-                            height: 30,
-                          ),
-                          borderRadius: BorderRadius.circular(4),
-                        ))
-                      ,
-                      const Text('赞过'),
-                      const Icon(Icons.chevron_right)
-                    ],
-                  )
-                ]))
-        ]));
+                  Row(children: [
+                    ClipRRect(
+                      child: Image.asset(
+                        'lib/assets/img/head.jpg',
+                        width: 30,
+                        height: 30,
+                      ),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    const Icon(Icons.chevron_right)
+                  ])
+                ],
+              ),
+              const Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(children: const [Icon(Icons.whatshot), Text('直播')]),
+                  Row(children: [
+                    ClipRRect(
+                      child: Image.asset(
+                        'lib/assets/img/head.jpg',
+                        width: 30,
+                        height: 30,
+                      ),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    const Icon(Icons.chevron_right)
+                  ])
+                ],
+              ),
+            ],
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 10),
+          color: Colors.white,
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(children: const [Icon(Icons.qr_code_scanner), Text('扫一扫')]),
+              Row(children: [
+                ClipRRect(
+                  child: Image.asset(
+                    'lib/assets/img/head.jpg',
+                    width: 30,
+                    height: 30,
+                  ),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                const Icon(Icons.chevron_right)
+              ])
+            ],
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 10),
+          color: Colors.white,
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(children: const [Icon(Icons.grid_view), Text('小程序')]),
+              Row(children: [
+                ClipRRect(
+                  child: Image.asset(
+                    'lib/assets/img/head.jpg',
+                    width: 30,
+                    height: 30,
+                  ),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                const Icon(Icons.chevron_right)
+              ])
+            ],
+          ),
+        ),
+      ]),
+    );
   }
 }
